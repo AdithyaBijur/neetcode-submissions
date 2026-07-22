6b7786cea95ca5067f1a4ac41e85a890c9c6bdb2
Adithya Bijur <38159749+AdithyaBijur@users.noreply.github.com>
2026-07-22 21:26:39 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, n: int, prerequisites: List[List[int]]) -> bool:
+
+        graph = collections.defaultdict(list)
+
+        for p in prerequisites:
+            graph[p[0]].append(p[1])
+
+        visited = {}
+        
+
+        def dfs(node):
+            if node in visited:
+                if visited[node] == 1:
+                    return False
+                if visited[node] == 2:
+                    return True       # already checked
+
+    
+            visited[node] = 1
+            for nei in graph[node]:
+                if not dfs(nei):
+                    return False
+            
+            visited[node] = 2
+            return True
+
+        for i in range(n):
+            if i not in visited and not dfs(i):
+                return False
+        return True
+
+        
