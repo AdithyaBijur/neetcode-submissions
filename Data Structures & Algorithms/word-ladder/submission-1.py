9acfc080dc1c0b446484cbb4f549950a808264ea
Adithya Bijur <38159749+AdithyaBijur@users.noreply.github.com>
2026-07-23 16:09:58 +0530

    Add: word-ladder - submission-1

---
+++ Data Structures & Algorithms/word-ladder/submission-1.py
@@ -0,0 +1,42 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        
+        patternMap = collections.defaultdict(list)
+        if endWord not in wordList:
+            return 0
+        if beginWord == endWord:
+            return 0
+        if beginWord not in wordList:
+            wordList.append(beginWord)
+        for word in wordList:
+            temp = list(word)
+            for i in range(0, len(word)):
+                temp[i] = '*'
+                patternMap[''.join(temp)].append(word)
+                temp[i] = word[i]
+        
+       
+ 
+        
+        que = collections.deque([beginWord])
+
+        level = 0
+        while que:  
+            print(que)
+            level += 1
+            n = len(que)
+            for _ in range(n):
+                word = que.popleft()
+                if word == endWord:
+                    return level
+                temp = list(word)
+                for i in range(0, len(word)):
+                    temp[i] = '*'
+                    pattern = ''.join(temp)
+                    ele = patternMap[pattern]
+                    patternMap[pattern] = [] #acts as visited
+                    for e in ele:
+                        que.append(e)
+                    temp[i] = word[i]
+        return 0
+                
